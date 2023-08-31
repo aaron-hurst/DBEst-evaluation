974a02fd486c0010a4a4e90f0be1c4636507274e
Aaron Hurst <ahhurst0@gmail.com>
2023-08-31 10:19:10 +0200

    Updates script for getting required models

--- get_list_of_used_models.py
+++ get_list_of_used_models.py
@@ -2,33 +2,54 @@ import os
 
 from config import QUERIES_DIR
 
-DATASET_ID = "usdot-flights_1b"
-QUERY_SET = 4
+DATASETS = {
+    "ampds-basement_plugs_and_lights": 1,
+    "ampds-current": 1,
+    "ampds-furnace_and_thermostat": 1,
+    "chicago-taxi_trips_2020": 1,
+    "kaggle-aquaponics": 1,
+    "kaggle-light_detection": 1,
+    "kaggle-smart_building_system": 1,
+    "kaggle-temperature_iot_on_gcp": 1,
+    "uci-gas_sensor_home_activity": 1,
+    "uci-household_power_consumption": 1,
+    "usdot-flights": 1,
+    # "uci-household_power_consumption": 15,
+    # "uci-household_power_consumption_synthetic": 15,
+    # "uci-household_power_consumption_10m": 15,
+    # "uci-household_power_consumption_100m": 15,
+    # "uci-household_power_consumption_1b": 15,
+    # "usdot-flights": 4,
+    # "usdot-flights_synthetic": 4,
+    # "usdot-flights_10m": 4,
+    # "usdot-flights_100m": 4,
+    # "usdot-flights_1b": 4,
+}
 
+for dataset_id, query_set in DATASETS.items():
+    print(f"Checking {dataset_id} set {query_set} for required models...")
+    queries_filepath = os.path.join(QUERIES_DIR, f"{dataset_id}_v{query_set}.txt")
+    with open(queries_filepath) as f:
+        queries = f.readlines()
+    out_filename = os.path.join(
+        "evaluation", f"models_required_{dataset_id}_v{query_set}.txt"
+    )
+    models = []
+    for i, query in enumerate(queries):
+        query_split = query.split(" ")
+        aggregation_column = query_split[1].split("(")[1].split(")")[0]
+        predicate_columns = [
+            query_split[i + 1]
+            for i in range(len(query_split))
+            if query_split[i].lower() in ["where", "and", "or"]
+        ]
+        total_columns = len(set([aggregation_column, *predicate_columns]))
+        if total_columns > 2:
+            continue
+        models.append(f"{aggregation_column},{list(set(predicate_columns))[0]}")
 
-queries_filepath = os.path.join(QUERIES_DIR, f"{DATASET_ID}_v{QUERY_SET}.txt")
-with open(queries_filepath) as f:
-    queries = f.readlines()
-out_filename = os.path.join(f"models_required_{DATASET_ID}_v{QUERY_SET}.txt")
-
-for i, query in enumerate(queries):
-    # Multi-column queries
-    query_split = query.split(" ")
-    aggregation_column = query_split[1].split("(")[1].split(")")[0]
-    predicate_columns = [
-        query_split[i + 1]
-        for i in range(len(query_split))
-        if query_split[i].lower() in ["where", "and", "or"]
-    ]
-    total_columns = len(set([aggregation_column, *predicate_columns]))
-    if total_columns > 2:
-        continue
-
-    # Unsupported aggregation functions
-    # aggregation = query_split[1].split("(")[0]
-    # if aggregation.upper() not in ["SUM", "COUNT", "AVG", "VAR"]:
-    #     continue
-
-    # Export model name
-    with open(out_filename, "a") as fp:
-        fp.write(f"{aggregation_column},{list(set(predicate_columns))[0]}\n")
+    # Export model names
+    models = sorted(list(set(models)))
+    with open(out_filename, "w") as fp:
+        for model in models:
+            fp.write(f"{model}\n")
